3912a7250294c41e56d63ede083618c54e61b0c9
Nilesh Patel <npatel@tacme.com>
2016-04-13 14:00:03 +0400

    Added Two new method /todos and //todos/:id

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
 var express = require('express');
 var app = express();
-
 var PORT = process.env.PORT || 3000;
+
+var arrData = [{
+	id: 1,
+	description: "Testing Api",
+	completed: false,
+},{
+	id: 2,
+	description: "Testing Api in postman",
+	completed: false,
+}];
 
 app.get("/", function(req,res){
 	res.send("Welcome to todo api");
 });
+
+
+app.get("/todos",function(req, res){
+	res.json(arrData);
+});
+
+app.get("/todos/:id",function(req, res){
+	var matchedObj;	
+	var todoId = req.params.id;
+
+	arrData.forEach(function(obj){
+		if(obj.id == todoId){
+			matchedObj = obj;
+		}
+	});
+
+	if(matchedObj){
+		res.json(matchedObj);
+	} else {
+		res.status(404).send("Record not found");
+	}
+
+});
+
+
 
 app.listen(PORT);
 
